716510d32acd0cd9185dfa6278ad1b43d46ce848
jackmford <jackmitchellfordyce@gmail.com>
2025-12-27 12:26:54 -0600

    write to history

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+  "fmt"
 	"os"
 	"time"
 )
@@ -21,6 +22,10 @@ type ActiveSession struct {
 }
 
 func startSession(category string) error {
+  if _, err := os.Stat("active.json"); err == nil {
+		fmt.Println("A session is already running. Stop it before starting a new one.")
+		return nil
+	}
   active := ActiveSession{
     Category: category,
     StartTime: time.Now(),
@@ -78,6 +83,53 @@ func stopSession() error {
 
 }
 
+func checkStatus() {
+	data, err := os.ReadFile("active.json")
+	if err != nil {
+		fmt.Println("No active session running.")
+		return
+	}
+
+	var active ActiveSession
+	json.Unmarshal(data, &active)
+
+	elapsed := time.Since(active.StartTime).Round(time.Second)
+	fmt.Printf("Currently tracking: %s\n", active.Category)
+	fmt.Printf("Time elapsed: %s\n", elapsed)
+}
+
 func main() {
+  if len(os.Args) < 2 {
+    fmt.Println("Usage tracker [start <category> | stop | status]")
+    return
+  }
+
+  command := os.Args[1]
+  
+  switch command {
+  case "start":
+    if len(os.Args) < 3 {
+      fmt.Println("Please provide a category (e.g. start programming)")
+    }
+    err := startSession(os.Args[2])
+    if err != nil {
+      fmt.Printf("Error starting session %v\n", err)
+    } else {
+      fmt.Printf("Started tracking: %s\n", os.Args[2])
+    }
+  case "stop": 
+    err := stopSession()
+    
+    if err != nil {
+      fmt.Printf("Error stopping session: %v\n", err)
+    } else {
+      fmt.Println("Session stopped and saved to history.")
+    }
+  case "status":
+    checkStatus()
+  default:
+    fmt.Println("Unknown command. Use: start, stop, status.")
+  }
+
 
 }
